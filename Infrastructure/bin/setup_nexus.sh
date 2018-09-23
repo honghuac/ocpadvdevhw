ab8a05effbec0871c810a1484222b17dd06eff46
honghuac <honghuac@gmail.com>
2018-09-23 22:33:20 +0800

    Removed backup of shellscripts and added sleep intervals to setup_nexus.sh

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -15,8 +15,11 @@ fi
 GUID=$1
 echo "Setting up Nexus in project $GUID-nexus"
 oc new-app sonatype/nexus3:latest -n $GUID-nexus
+sleep 5s;
 oc expose svc nexus3
+sleep 5s;
 oc rollout pause dc nexus3
+sleep 5s;
 oc patch dc nexus3 --patch='{ "spec": { "strategy": { "type": "Recreate" }}}'
 oc set resources dc nexus3 --limits=memory=2Gi --requests=memory=1Gi
 echo "apiVersion: v1
@@ -29,13 +32,16 @@ spec:
   resources:
     requests:
       storage: 4Gi" | oc create -f -
+sleep 5s;
 oc set volume dc/nexus3 --add --overwrite --name=nexus3-volume-1 --mount-path=/nexus-data/ --type persistentVolumeClaim --claim-name=nexus-pvc
 oc set probe dc/nexus3 --liveness --failure-threshold 3 --initial-delay-seconds 60 -- echo ok
 oc set probe dc/nexus3 --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8081/repository/maven-public/
 oc rollout resume dc nexus3
+sleep 5s;
 curl -o setup_nexus3.sh -s https://raw.githubusercontent.com/wkulhanek/ocp_advanced_development_resources/master/nexus/setup_nexus3.sh
 chmod +x setup_nexus3.sh
 ./setup_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}')
 rm setup_nexus3.sh
 oc expose dc nexus3 --port=5000 --name=nexus-registry
+sleep 5s;
 oc create route edge nexus-registry --service=nexus-registry --port=5000
