073237d995551c9e992e17093a9350e2171a8089
honghuac <honghuac@gmail.com>
2018-09-27 11:58:43 +0800

    Created setup_grading_jenkins.sh and updated setup_jenkins.sh

--- Infrastructure/bin/setup_grading_jenkins.sh
+++ Infrastructure/bin/setup_grading_jenkins.sh
@@ -1,42 +1,29 @@
 #!/bin/bash
-# Setup Jenkins Project
+# Setup Grading Jenkins Project
 
-# Code to set up the Jenkins project to execute the
-# three pipelines.
-# This will need to also build the custom Maven Slave Pod
-# Image to be used in the pipelines.
-# Finally the script needs to create three OpenShift Build
-# Configurations in the Jenkins Project to build the
-# three micro services. Expected name of the build configs:
-# * mlbparks-pipeline
-# * nationalparks-pipeline
-# * parksmap-pipeline
+# Code to set up the Jenkins project to execute the grading Jenkins pipeline
 # The build configurations need to have two environment variables to be passed to the Pipeline:
 # * GUID: the GUID used in all the projects
 # * CLUSTER: the base url of the cluster used (e.g. na39.openshift.opentlc.com)
 
 # To be Implemented by Student
 
-
 #Create a Jenkins instance with persistent storage and sufficient resources
 #Create a build configuration to build the custom Maven slave pod to include Skopeo
-#Set up three build configurations with pointers to the pipelines in the source code project.
-#Each build configuration needs to point to the source code repository and the respective contextDir. The build configurations also need the following environment variables:
+#Add permission to allow grading-jenkins to view other projects
 #GUID: The common GUID for all projects
 #CLUSTER: The cluster base URL—for example, na39.openshift.opentlc.com
 
 
-
 if [ "$#" -ne 3 ]; then
     echo "Usage:"
-    echo "  $0 GUID REPO CLUSTER"
-    echo "  Example: $0 wkha https://github.com/wkulhanek/ParksMap na39.openshift.opentlc.com"
+    echo "  $0 GUID CLUSTER"
+    echo "  Example: $0 wkha na39.openshift.opentlc.com"
     exit 1
 fi
 
 GUID=$1
-REPO=$2
-CLUSTER=$3
+CLUSTER=$2
 echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
 
 #a. Create Jenkins app - without correct imagestream
@@ -56,14 +43,3 @@ docker build . -t docker-registry-default.apps.${GUID}.openshift.opentlc.com/${G
 sleep 20s;
 docker login -u opentlc-mgr -p $(oc whoami -t) docker-registry-default.apps.${GUID}.openshift.opentlc.com
 docker push docker-registry-default.apps.$GUID.openshift.opentlc.com/${GUID}-jenkins/jenkins-slave-maven-appdev:v3.10
-sleep 20s;
-
-#d. Create build config for 3 Jenkin Pipeline apps
-oc create -f $HOME/infrastructure/templates/setup_jenkins/mlbparks-pipeline.yaml
-sleep 5s;
-
-oc create -f $HOME/infrastructure/templates/setup_jenkins/nationalparks-pipeline.yaml
-sleep 5s;
-
-oc create -f $HOME/infrastructure/templates/setup_jenkins/parksmap-pipeline.yaml
-sleep 5s;
