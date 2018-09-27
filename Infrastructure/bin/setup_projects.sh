bce56759817f60f6961fc404f909157007f5c387
honghuac <honghuac@gmail.com>
2018-09-27 19:08:34 +0800

    Updated setup_projects.sh

--- Infrastructure/bin/setup_projects.sh
+++ Infrastructure/bin/setup_projects.sh
@@ -16,17 +16,15 @@ oc new-project ${GUID}-jenkins    --display-name="${GUID} AdvDev Homework Jenkin
 oc new-project ${GUID}-parks-dev  --display-name="${GUID} AdvDev Homework Parks Development"
 oc new-project ${GUID}-parks-prod --display-name="${GUID} AdvDev Homework Parks Production"
 
-oc adm policy add-cluster-role-to-user self-provisioner system:serviceaccount:grading-jenkins:jenkins
-oc describe rolebinding.rbac
-
 oc policy add-role-to-user admin ${USER} -n ${GUID}-nexus
 oc policy add-role-to-user admin ${USER} -n ${GUID}-sonarqube
 oc policy add-role-to-user admin ${USER} -n ${GUID}-jenkins
 oc policy add-role-to-user admin ${USER} -n ${GUID}-parks-dev
 oc policy add-role-to-user admin ${USER} -n ${GUID}-parks-prod
 
-oc annotate namespace ${GUID}-nexus      openshift.io/requester=${USER} --overwrite
-oc annotate namespace ${GUID}-sonarqube  openshift.io/requester=${USER} --overwrite
-oc annotate namespace ${GUID}-jenkins    openshift.io/requester=${USER} --overwrite
-oc annotate namespace ${GUID}-parks-dev  openshift.io/requester=${USER} --overwrite
-oc annotate namespace ${GUID}-parks-prod openshift.io/requester=${USER} --overwrite
+#TO-DO
+#oc annotate namespace ${GUID}-nexus      openshift.io/requester=${USER} --overwrite
+#oc annotate namespace ${GUID}-sonarqube  openshift.io/requester=${USER} --overwrite
+#oc annotate namespace ${GUID}-jenkins    openshift.io/requester=${USER} --overwrite
+#oc annotate namespace ${GUID}-parks-dev  openshift.io/requester=${USER} --overwrite
+#oc annotate namespace ${GUID}-parks-prod openshift.io/requester=${USER} --overwrite
