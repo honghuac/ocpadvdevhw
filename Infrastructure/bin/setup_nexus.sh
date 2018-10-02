16b6f6967df65a5a5d00339c7bc281616616a92b
honghuac <honghuac@gmail.com>
2018-10-02 16:42:57 +0800

    Updated setup_nexus script

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -15,38 +15,26 @@ fi
 GUID=$1
 echo "Setting up Nexus in project $GUID-nexus"
 
-oc new-app sonatype/nexus3:latest -n $GUID-nexus
-
-sleep 15s;
-
-oc expose svc/nexus3 -n $GUID-nexus
-
-sleep 5s;
-
-oc rollout pause dc/nexus3 -n $GUID-nexus
-
-sleep 5s;
-
-oc patch dc/nexus3 --patch='{ "spec": { "strategy": { "type": "Recreate" }}}' -n $GUID-nexus
-
-oc set resources dc/nexus3 --limits=memory=2Gi --requests=memory=1Gi -n $GUID-nexus
-
-oc create -f "./Infrastructure/templates/setup_nexus/nexus.yaml" -n $GUID-nexus
-
-sleep 5s;
-
-echo "Setting PV and probes in project $GUID-nexus"
-
-oc set volume dc/nexus3 --add --overwrite --name=nexus3-volume-1 --mount-path=/nexus-data/ --type persistentVolumeClaim --claim-name=nexus-pvc -n $GUID-nexus
-
-oc set probe dc/nexus3 --liveness --failure-threshold 3 --initial-delay-seconds 60 -n $GUID-nexus -- echo ok
-
-oc set probe dc/nexus3 --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8081/repository/maven-public/ -n $GUID-nexus
-
-oc rollout resume dc nexus3 -n $GUID-nexus
-
-sleep 5m;
-
 #stuff goes here
+
+wget https://raw.githubusercontent.com/wkulhanek/ocp_advanced_development_resources/master/nexus/setup_nexus3.sh
+
+chmod +x setup_nexus3.sh
+
+echo "Running setup_nexus3 script in $GUID-nexus"
+
+./setup_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}' -n $GUID-nexus)
+
+sleep 10s;
+
+rm setup_nexus3.sh*
+
+oc expose dc/nexus3 --port=5000 --name=nexus-registry -n $GUID-nexus
+
+oc create route edge nexus-registry --service=nexus-registry --port=5000 -n $GUID-nexus
+
+oc annotate route nexus3 console.alpha.openshift.io/overview-app-route=true
+
+oc annotate route nexus-registry console.alpha.openshift.io/overview-app-route=false
 
 echo "Completed setting up Nexus in project $GUID-nexus"
